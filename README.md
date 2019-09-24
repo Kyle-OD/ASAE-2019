3d709cbb6fe1fe0c9231c716790135a517cfed84
Kyle-OD <kpod@udel.edu>
2019-09-24 14:31:32 -0400

    updated readme with weekly goals, added pinout to radio test

--- README.md
+++ README.md
@@ -12,6 +12,12 @@ Kyle O'Donnell, Brendan Jones, Luis Marcano, Matt Heebner
 
 The files found in this repository are currently from the previous year's team, and will be updated and improved upon as we get our hardware in and figure out exacty what how we will need to code the Data Acquisition System and onboard receiver.
 
+## Current Objectives
+
+* Continue reading code, finding areas that can be imporved upon
+
+* Write test code for DAS radio system.  Currently waiting on hardware to arrive.
+
 ## 2020 Rules and Regulations
 
 The rules for the 2020 Competition can be found at the following link, under the name "2020 SAE Aero Design Rules" : [Rules](https://www.saeaerodesign.com/cdsweb/gen/DocumentResources.aspx)
--- Radio_Test.ino
+++ Radio_Test.ino
@@ -1,2 +1,20 @@
 #include <RH_RF95.h> //LoRa Radio library
 
+/* Radio Module Pinout (Not Arduino Pin Numbers)
+    PIN 1 - GND
+    PIN 2 - MISO        : SPI Data Output
+    PIN 3 - MOSI        : SPI Data Input
+    PIN 4 - SCK         : SPI Clock Input
+    PIN 5 - NSS         : SPI Chip Select Input
+    PIN 6 - RESET       : Reset Trigger Input
+    PIN 7 - DIO5        : Digital I/O, software configured
+    PIN 8 - GND
+    PIN 9 - ANT         : RF Signal Output
+    PIN 10 - GND
+    PIN 11 - DIO3       : Digital I/O, software configured
+    PIN 12 - DIO4       : Digital I/O, software configured
+    PIN 13 - 3.3V       : Supply Voltage
+    PIN 14 - DIO0       : Digital I/O, software configured
+    PIN 15 - DIO1       : Digital I/O, software configured
+    PIN 16 - DIO2       : Digital I/O, software configured
+ */
